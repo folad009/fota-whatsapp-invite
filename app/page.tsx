05172a77e55dfd04323ecbef7b6e8239df1c0708
Fola <folaremidixon@gmail.com>
2026-08-13 17:19:36 +0100

    resolvethe page issue

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 // import { FeatureGrid } from "@/components/landing/FeatureGrid";
 // import { HowItWorks } from "@/components/landing/HowItWorks";
 // import { LandingCta } from "@/components/landing/LandingCta";
-import { LandingFooter } from "@/components/landing/LandingFooter";
+// import { LandingFooter } from "@/components/landing/LandingFooter";
 import { LandingHero } from "@/components/landing/LandingHero";
 import { LandingNavbar } from "@/components/landing/LandingNavbar";
 
